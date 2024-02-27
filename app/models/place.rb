0682db552604c047016b36977080098a4135a4cd
Katherine NewportForbes <katherinenf@gmail.com>
2024-02-27 12:11:57 +0100

    remove duplicate place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,7 @@
 class Place < ApplicationRecord
-
   belongs_to :user
   has_many :bookings
 
-  validates :title,  presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :price, :description, :address, presence: true
-
 end
