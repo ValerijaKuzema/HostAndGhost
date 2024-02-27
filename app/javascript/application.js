f4732f316af4269a5961579263a16b85b2d2d7e4
Valerija Kuzema <valerija_kuzema@web.de>
2024-02-27 20:26:22 +0100

    fine tuning navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+
+document.addEventListener('scroll', function() {
+      const navbar = document.getElementById('header_navbar');
+      if (window.scrollY > 50) {
+        navbar.classList.add('small');
+      } else {
+        navbar.classList.remove('small');
+      }
+    });
