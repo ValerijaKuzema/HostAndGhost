8d9da95955c7afaa0a11bfbeeccd0101f801cf86
Dinusha Hansamali Liyanarathna Herasinghe <dinushaasb111@gmail.com>
2024-02-27 11:45:27 +0100

    Update place.rb
    Changed name to title in Place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,6 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :name,  presence: true, uniqueness: true
+  validates :title,  presence: true, uniqueness: true
   validates :price, :description, :address, presence: true
 end
