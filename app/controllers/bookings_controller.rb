5d9c1bb8349d5a327f041c44111eec5f01573fe6
Katherine NewportForbes <katherinenf@gmail.com>
2024-02-28 15:32:13 +0100

    bookings controller new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,28 @@
 class BookingsController < ApplicationController
+  before_action :set_place, only: [:create]
+
   def index
     @bookings = Booking.where(user: current_user)
   end
 
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.place = @place
+    @booking.user = current_user
+    if @booking.save!
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:place_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start, :end)
+  end
 end
