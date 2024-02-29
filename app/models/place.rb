cc84e5584af70311800538cc3635b3ebc7633189
Valerija Kuzema <valerija_kuzema@web.de>
2024-02-29 16:05:02 +0100

    blank map on our homepage so far

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,8 @@ class Place < ApplicationRecord
   has_many :bookings
   has_many :reviews
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   validates :title, presence: true, uniqueness: true
   validates :price, :description, :address, presence: true
 end
