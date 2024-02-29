e00e821294fcf6bd4d580498bbe937c71f3cd718
DinushaHansamali LiyanarathnaHerasinghe <dinushaasb111@gmail.com>
2024-02-29 12:59:27 +0100

    saving_reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,14 @@ class ReviewsController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @review = Review.new(review_params)
+    @review.user = current_user
     @review.place = @place
     if @review.save
+      puts "review #{@review}"
       redirect_to place_path(@place)
     else
-      render "places/show", status: :unprocessable_entity
+      puts "review error #{@review}"
+      render :new, status: :unprocessable_entity
     end
   end
 
